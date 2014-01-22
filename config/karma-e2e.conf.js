d63e773a98405cd8b5eae8b768a326388ddbdfc3
Florent Vielfaure <florent.vielfaure@gmail.com>
2014-01-22 22:38:55 +0100

    fix tests bugs, but still a problem with browser.location.url

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -20,6 +20,8 @@ module.exports = function(config){
       '/': 'http://localhost:8000/'
     },
 
+    urlRoot : '/',
+
     plugins : [
             'karma-junit-reporter',
             'karma-chrome-launcher',
