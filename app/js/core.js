3ce8882d24a18eae7c83ef8d548f8a97b683122c
Florent Vielfaure <florent.vielfaure@gmail.com>
2014-01-30 14:55:13 +0100

    - modification of constructor parameters for more flexibility  - refactoring of core tests

--- app/js/core.js
+++ app/js/core.js
@@ -13,19 +13,15 @@ function inheritPrototype(childObject, parentObject) {
 }
 
 // Extention of google.maps.Marker
-function PointOfInterest(position, name, type, description, map) {
-    google.maps.Marker.call(this, {
-        position: position,
-        title: name,
-        map: map,
-        clickable: false,
-        crossOnDrag: false,
-        raiseOnDrag: false,
-        draggable: false,
-        flat: true
-    });
-    this.type = type;
-    this.description = description;
+function PointOfInterest(args) {
+    if (typeof args.clickable === 'undefined') args.clickable = false;
+    if (typeof args.crossOnDrag === 'undefined') args.crossOnDrag = false;
+    if (typeof args.raiseOnDrag === 'undefined') args.raiseOnDrag = false;
+    if (typeof args.draggable === 'undefined') args.draggable = false;
+    if (typeof args.flat === 'undefined') args.flat = true;
+    google.maps.Marker.call(this, args);
+    if (typeof args.type !== 'undefined') this.type = args.type;
+    if (typeof args.description !== 'undefined') this.description = args.description;
 }
 inheritPrototype(PointOfInterest, google.maps.Marker);
 PointOfInterest.prototype.getType = function() {
@@ -36,28 +32,56 @@ PointOfInterest.prototype.getDescription = function() {
 }
 
 // Extention of google.maps.Polyline
-function Track(path, name, type, description, color, opacity, weight, map) {
-    google.maps.Polyline.call(this, {
-        strokeColor: color,
-        strokeOpacity: opacity,
-        strokeWeight: weight,
-        path: path,
-        map: map,
-        clickable: false,
-        draggable: false,
-        editable: false
-    });
-    this.name = name;
-    this.type = type;
-    this.description = description;
+function Track(args) {
+    if (typeof args.clickable === 'undefined') args.clickable = false;
+    if (typeof args.draggable === 'undefined') args.draggable = false;
+    if (typeof args.editable === 'undefined') args.editable = false;
+    google.maps.Polyline.call(this, args);
+    if (typeof args.title !== 'undefined') this.title = args.title;
+    if (typeof args.type !== 'undefined') this.type = args.type;
+    if (typeof args.description !== 'undefined') this.description = args.description;
 }
 inheritPrototype(Track, google.maps.Polyline);
 Track.prototype.getTitle = function() {
-    return this.name;
+    return this.title;
 }
 Track.prototype.getType = function() {
     return this.type;
 }
 Track.prototype.getDescription = function() {
     return this.description;
+}
+
+// Container for the whole route
+function Route(args) {
+    if (typeof args.title !== 'undefined') this.title = args.title;
+    if (typeof args.type !== 'undefined') this.type = args.type;
+    if (typeof args.difficulty !== 'undefined') this.difficulty = args.difficulty;
+    if (typeof args.interest !== 'undefined') this.interest = args.interest;
+    if (typeof args.description !== 'undefined') this.description = args.description;
+    if (typeof args.tracks !== 'undefined' &&
+        Array.isArray(args.tracks)) this.tracks = args.tracks;
+    if (typeof args.pois !== 'undefined' &&
+        Array.isArray(args.pois)) this.pois = args.pois;
+}
+Route.prototype.getTitle = function() {
+    return this.title;
+}
+Route.prototype.getType = function() {
+    return this.type;
+}
+Route.prototype.getDifficulty = function() {
+    return this.difficulty;
+}
+Route.prototype.getInterest = function() {
+    return this.interest;
+}
+Route.prototype.getDescription = function() {
+    return this.description;
+}
+Route.prototype.getTracks = function() {
+    return this.tracks;
+}
+Route.prototype.getPois = function() {
+    return this.pois;
 }
--- test/unit/coreSpec.js
+++ test/unit/coreSpec.js
@@ -7,31 +7,69 @@ describe('core', function(){
   	var map = new google.maps.Map(document.createElement('div'), {} );
 	var myLatlng = new google.maps.LatLng(-25,130);
 
-	it('should say the map exists', function() {
-		expect(typeof map).toBe('object');
-	});
-	
-	it('should say the coordinates are (-25,130)', function() {
-		expect(myLatlng.lat()).toBe(-25);
-		expect(myLatlng.lng()).toBe(130);
-	});
+	describe('google.maps.api (quick tests)', function() {
+		it('should say the map exists', function() {
+			expect(typeof map).toBe('object');
+		});
+		
+		it('should say the coordinates are (-25,130)', function() {
+			expect(myLatlng.lat()).toBe(-25);
+			expect(myLatlng.lng()).toBe(130);
+		});
+	})
 
-	it('should create a PointOfInterest', function() {
-		var a = new PointOfInterest(myLatlng, "Name", false, "Description", map);
-		expect(typeof a).toBe('object');
-		expect(a.getTitle()).toBe('Name');
-		expect(a.getType()).toBe(false);
-		expect(a.getDescription()).toBe('Description');
-		expect(a.getMap()).toBe(map);
-	});
+	describe('PointOfInterest', function() {
+		it('should create a PointOfInterest', function() {
+			var a = new PointOfInterest({
+				position: myLatlng, 
+				title: "Name", 
+				type: false, 
+				description: "Description", 
+				map: map
+			});
+			expect(typeof a).toBe('object');
+			expect(a.getTitle()).toBe('Name');
+			expect(a.getType()).toBe(false);
+			expect(a.getDescription()).toBe('Description');
+			expect(a.getMap()).toBe(map);
+		});
+	})
 
-	it('should create a Track', function() {
-		var a = new Track(new google.maps.MVCArray(), "Name", false, "Description", "#ffffff", 1, 1, map);
-		expect(typeof a).toBe('object');
-		expect(a.getTitle()).toBe('Name');
-		expect(a.getType()).toBe(false);
-		expect(a.getDescription()).toBe('Description');
-		expect(a.getMap()).toBe(map);
-	});
+	describe('Track', function() {
+		it('should create a Track', function() {
+			var a = new Track({
+				title: "Name", 
+				type: false, 
+				description: "Description", 
+				strokeColor: "#ffffff", 
+				strokeOpacity: 1, 
+				strokeWeight: 1, 
+				map: map
+			});
+			expect(typeof a).toBe('object');
+			expect(a.getTitle()).toBe('Name');
+			expect(a.getType()).toBe(false);
+			expect(a.getDescription()).toBe('Description');
+			expect(a.getMap()).toBe(map);
+		});
+	})
+
+	describe('Route', function() {
+		it('should create a Route', function() {
+			var a = new Route({
+				title: "Name", 
+				type: false, 
+				description: "Description", 
+				difficulty: 3, 
+				interest: 1
+			});
+			expect(typeof a).toBe('object');
+			expect(a.getTitle()).toBe('Name');
+			expect(a.getType()).toBe(false);
+			expect(a.getDescription()).toBe('Description');
+			expect(a.getDifficulty()).toBe(3);
+			expect(a.getInterest()).toBe(1);
+		});
+	})
 
 });
