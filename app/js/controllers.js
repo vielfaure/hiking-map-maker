952569633aac36e7adfd7349697e6e03a99d34e0
Florent Vielfaure <florent.vielfaure@gmail.com>
2014-02-26 16:57:23 +0100

    major changes in layout (no responsive), dummy map added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,12 @@
 /* Controllers */
 
 angular.module('hmmApp.controllers', []).
-  controller('MapCtrl', [function() {
+  controller('MapCtrl', [function($scope) {
+  	var mapOptions = {
+        zoom: 6,
+        center: new google.maps.LatLng(46.7481387, 1.7021013),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
 
+    $scope.map = new google.maps.Map(document.getElementById('map-canvas-map'), mapOptions);
   }]);
